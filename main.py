07c60ca91758a04ec7902acf0da22d21250fd0f9
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-26 18:22:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+n1 = int(input("Digite um número: "))
+
+if (n1 > 10):
+  print("O número é maior que 10")
+elif (n1 == 10):
+  print("O número é igual a 10")
+elif (n1 < 10):
+  print ("O número é menor que 10")
